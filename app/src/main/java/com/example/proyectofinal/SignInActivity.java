50bf6d78a173ed662832b10c65c2fe6906cd40ca
Michael <mich.montes@gmail.com>
2019-04-24 19:30:26 -0600

    correccion de api facebook, no funcional local con volley se tuvo que subir a heroku para funcionar

--- app/src/main/java/com/example/proyectofinal/SignInActivity.java
+++ app/src/main/java/com/example/proyectofinal/SignInActivity.java
@@ -3,12 +3,21 @@ package com.example.proyectofinal;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -22,6 +31,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -69,17 +80,19 @@ public class SignInActivity extends AppCompatActivity {
 
                 if (AccessToken.getCurrentAccessToken() == null) {
                     LoginManager.getInstance().logInWithReadPermissions(SignInActivity.this, Arrays.asList("public_profile", "email"));
+                }else{
+
+                    ColorDrawable customerbuttonColor = (ColorDrawable) customerButton.getBackground();
+                    if(customerbuttonColor.getColor() == getResources().getColor(R.color.colorAccent)){
+                        loginToServer(AccessToken.getCurrentAccessToken().getToken(), "customer");
+                        Log.d("CUSTOMER 1", "CUSTOMER 1");
+                    }else{
+                        loginToServer(AccessToken.getCurrentAccessToken().getToken(), "driver");
+                        Log.d("DRIVER 1", "DRIVER 1");
+                    }
                 }
 
-//                ColorDrawable customerbuttonColor = (ColorDrawable) customerButton.getBackground();
-//
-//                if(customerbuttonColor.getColor() == getResources().getColor(R.color.colorAccent)){
-//                    Intent intent = new Intent(getApplicationContext(), CustomerActivity.class);
-//                    startActivity(intent);
-//                }else{
-//                    Intent intent = new Intent(getApplicationContext(), DriverActivity.class);
-//                    startActivity(intent);
-//                }
+
 
             }
         });
@@ -126,6 +139,15 @@ public class SignInActivity extends AppCompatActivity {
                         parameters.putString("fields", "id,name,email,picture");
                         request.setParameters(parameters);
                         request.executeAsync();
+
+                        ColorDrawable customerbuttonColor = (ColorDrawable) customerButton.getBackground();
+                        if(customerbuttonColor.getColor() == getResources().getColor(R.color.colorAccent)){
+                            loginToServer(AccessToken.getCurrentAccessToken().getToken(), "customer");
+                            Log.d("CUSTOMER 2", "CUSTOMER 2");
+                        }else{
+                            loginToServer(AccessToken.getCurrentAccessToken().getToken(), "driver");
+                            Log.d("DRIVER 2", "DRIVER 2");
+                        }
                     }
 
                     @Override
@@ -163,5 +185,67 @@ public class SignInActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+    private void loginToServer(String facebookAccessToken, final String userType) {
+        String url = "https://boiling-mesa-85590.herokuapp.com/api/social/convert-token";
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("grant_type", "convert_token");
+            jsonBody.put("client_id", "GDUdJrHqsgE8UpPHvhpqMwoUlpwix4Wh90fHuFvJ");
+            jsonBody.put("client_secret", "ThVjmbuhmslQKn2ae3LCMTLRgV7Ca3cAfI7lWdPmOzk4z2jI5xucwB37hWrL0IxsgMUjGEVVg3y0mLwvbrtbEAu1qw0XiPPAfWS6bNfdTamYV9UFDeNLF1Ej22ZnIEkJ");
+            jsonBody.put("backend", "facebook");
+            jsonBody.put("token", facebookAccessToken);
+            jsonBody.put("user_type", userType);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d("ERROR EN JSONBODY", "ERROR EN JSONBODY");
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Execute code
+                        Log.d("LOGIN TO SERVER", response.toString());
+
+                        // Shave server token to local database
+                        SharedPreferences.Editor editor = sharedPref.edit();
+
+                        try {
+                            editor.putString("token", response.getString("access_token"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        editor.commit();
+
+                        // Start main activity
+                        if (userType.equals("customer")) {
+                            Intent intent = new Intent(getApplicationContext(), CustomerActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(getApplicationContext(), DriverActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.d("EonErrorResponse tttY", error.toString());
+
+                    }
+                });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+
+
     }
 }
