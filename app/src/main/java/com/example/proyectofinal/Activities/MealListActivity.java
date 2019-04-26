b9e72becfc9ea1b21e271dcdfeb1379cf6ff06b7
Michael <mich.montes@gmail.com>
2019-04-26 13:42:44 -0600

    Pantalla de comidas

--- app/src/main/java/com/example/proyectofinal/Activities/MealListActivity.java
+++ app/src/main/java/com/example/proyectofinal/Activities/MealListActivity.java
@@ -3,6 +3,7 @@ package com.example.proyectofinal.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +11,29 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.proyectofinal.Adapters.MealAdapter;
+import com.example.proyectofinal.Objects.Meal;
+
 import com.example.proyectofinal.R;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MealListActivity extends AppCompatActivity {
+
+    private ArrayList<Meal> mealArrayList;
+    private MealAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,35 +46,59 @@ public class MealListActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle(restaurantName);
 
+
+        mealArrayList = new ArrayList<Meal>();
+        adapter = new MealAdapter(this, mealArrayList, restaurantId);
+
         ListView listView =  findViewById(R.id.meal_list);
-        listView.setAdapter(new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 3;
-            }
+        listView.setAdapter(adapter);
 
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
 
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                return LayoutInflater.from(MealListActivity.this).inflate(R.layout.list_item_meal, null);
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MealListActivity.this, MealDetailActivity.class );
-                startActivity(intent);
-            }
-        });
+        //Obtener lista de comidas
+        getMeals(restaurantId);
     }
+
+    private void getMeals(String restaurantId) {
+        String url = getString(R.string.API_URL) + "/customer/meals/" + restaurantId + "/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("LISTA DE COMIDAS", response.toString());
+
+                        // Convert JSON data to JSON Array
+                        JSONArray mealsJSONArray = null;
+
+                        try {
+                            mealsJSONArray = response.getJSONArray("meals");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Convert Json Array to Restaurant Array
+                        Gson gson = new Gson();
+                        Meal[] meals = gson.fromJson(mealsJSONArray.toString(), Meal[].class);
+
+                        // Refresh ListView with up-to-date data
+                        mealArrayList.clear();
+                        mealArrayList.addAll(new ArrayList<Meal>(Arrays.asList(meals)));
+                        adapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+    }
+
 }
